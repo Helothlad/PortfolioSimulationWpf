0dd037ec89a5b6509870c21c4c3461bf548ece14
Chris <janosi.krisztian02@gmail.com>
2025-04-14 17:17:49 +0200

    MainFormBasicLayout

---
+++ PortfolioSimulationWpf/PnLToBrushConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PortfolioSimulationWpf
+{
+    internal class PnLToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal pnl = (decimal)value;
+            if(pnl > 0)
+            {
+                return Brushes.Green;
+            }else if(pnl < 0)
+            {
+                return Brushes.Red;
+            }
+            else
+            {
+                return Brushes.Black;
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PortfolioSimulationWpf/ViewModel.cs
+++ PortfolioSimulationWpf/ViewModel.cs
@@ -9,10 +9,14 @@ namespace PortfolioSimulationWpf
 {
     internal class ViewModel : INotifyPropertyChanged
     {
+        
         public event PropertyChangedEventHandler? PropertyChanged;
         public BindingList<Asset> Assets { get; private set; }
         public Asset? SelectedAsset { get; set; }
+        public decimal Cash { get; private set; }
         public decimal TotalAssetsValue => Assets.Sum(a => a.TotalValue);
+        public decimal TotalPnl => Assets.Sum(a => a.PnL);
+        public decimal TotalValue => Cash + TotalAssetsValue;
         public ViewModel()
         {
             this.Assets = new BindingList<Asset>()
@@ -21,6 +25,7 @@ namespace PortfolioSimulationWpf
                 new Asset("BTC", AssetType.Crypto, 1),
                 new Asset("SPY", AssetType.ETF, 5)
             };
+            Cash = new decimal(3000);
         }
         public void SimulateDay()
         {
@@ -29,6 +34,8 @@ namespace PortfolioSimulationWpf
                 asset.Simulate();
             }
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TotalAssetsValue)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TotalPnl)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TotalValue)));
         }
     }
 }
